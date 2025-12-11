5b3df3f3f4972a261d3fee949eb9ae97e9a830f5
Student Dropout Predictor Developer <developer@studentpredictor.com>
2025-12-11 15:26:50 +0530

    Clear repository contents

--- Question2.py
+++
@@ -1,7 +0,0 @@
-import cv2, numpy as np
-from sklearn.cluster import KMeans
-
-img = cv2.imread("Dog.jpg"); h,w = img.shape[:2]; X = img.reshape(-1,3)
-k = KMeans(n_clusters=16).fit(X)
-out = k.cluster_centers_[k.labels_].astype("uint8").reshape(h,w,3)
-cv2.imwrite("compressed.png", out)
